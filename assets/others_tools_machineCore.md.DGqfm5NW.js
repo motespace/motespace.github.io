63e7f8822d1f9ffe46417f9ae953734d4c562795
motespace <motespace@users.noreply.github.com>
2026-09-05 13:19:04 +0000

    deploy: motespace/mote-notes@a8edca68ae3e8c32ef7e786a5cdf13bdffb5fb7f

---
+++ assets/others_tools_machineCore.md.DGqfm5NW.js
@@ -0,0 +1,19 @@
+import{_ as s,o as a,c as n,a5 as e}from"./chunks/framework.Bt0SrEfo.js";const o=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"others/tools/machineCore.md","filePath":"others/tools/machineCore.md","lastUpdated":1788613730000}'),l={name:"others/tools/machineCore.md"};function p(i,t,d,r,g,h){return a(),n("div",null,[...t[0]||(t[0]=[e(`<table tabindex="0"><thead><tr><th></th><th style="text-align:right;"></th><th style="text-align:right;">价格 （默认jd ） 均为官方旗舰店</th></tr></thead><tbody><tr><td>CPU</td><td style="text-align:right;">i5-13600kf</td><td style="text-align:right;">1999</td></tr><tr><td>主板</td><td style="text-align:right;">ROG STRIX B70-G GAMING WIFI 小吹雪 D4</td><td style="text-align:right;">1279</td></tr><tr><td>显卡</td><td style="text-align:right;">微星MSI RX6650XT MECH 2X 8G</td><td style="text-align:right;">1779 （tb）</td></tr><tr><td>内存</td><td style="text-align:right;">光威 DDR4 3600 32Gx2</td><td style="text-align:right;">779</td></tr><tr><td>固态</td><td style="text-align:right;">致态ZhiTai TiPlus7100 1T PCle4.0</td><td style="text-align:right;">399</td></tr><tr><td>散热</td><td style="text-align:right;">利民 PA120 6热管双塔</td><td style="text-align:right;">179</td></tr><tr><td>电源</td><td style="text-align:right;">九州风神 DQ750M-VL 750w</td><td style="text-align:right;">459</td></tr><tr><td>机箱</td><td style="text-align:right;">爱国者 YOGO M4 PRO 白色</td><td style="text-align:right;">239</td></tr><tr><td>合计</td><td style="text-align:right;"></td><td style="text-align:right;">7112</td></tr></tbody></table><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>CPU    i5-13600kf   </span></span>
+<span class="line"><span>主板    ROG STRIX B70-G GAMING WIFI 小吹雪 D4    3138元</span></span>
+<span class="line"><span>显卡    RTX 2060super 矿卡                       1011元</span></span>
+<span class="line"><span>内存    光威 DDR4 3600  32Gx2         779元</span></span>
+<span class="line"><span>固态    致态 TiPlus7100 1T PCle4.0    399元</span></span>
+<span class="line"><span>散热    利民 PA120 6热管双塔      179元</span></span>
+<span class="line"><span>电源    九州风神 DQ750M-VL 750w   459元</span></span>
+<span class="line"><span>机箱    爱国者 YOGO M4 PRO 白色   239元</span></span>
+<span class="line"><span>6300左右</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>CPU     i5-13600kf </span></span>
+<span class="line"><span>主板    ROG STRIX B70-G GAMING WIFI 小吹雪 D4 </span></span>
+<span class="line"><span>显卡    微星MSI RX6650XT MECH 2X 8G</span></span>
+<span class="line"><span>内存    光威 DDR4 3600  32Gx2  </span></span>
+<span class="line"><span>固态    致态 TiPlus7100 1T PCle4.0 </span></span>
+<span class="line"><span>散热    利民 PA120 6热管双塔 </span></span>
+<span class="line"><span>电源    九州风神 DQ750M-VL 750w </span></span>
+<span class="line"><span>机箱    爱国者 YOGO M4 PRO 白色</span></span></code></pre></div>`,2)])])}const x=s(l,[["render",p]]);export{o as __pageData,x as default};
